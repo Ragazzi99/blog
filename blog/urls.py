72b8337061abe2e17554e06381cd708d5bb893cd
Ragazzi99 <victoreze707@yahoo.com>
2022-09-27 13:31:27 +0100

    fola

--- blog/urls.py
+++ blog/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('victor/', admin.site.urls),
     path('', include ('home.urls'))
 ]
 if settings.DEBUG:
